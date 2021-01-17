37f9cdca215f4c9b9950395024f6855d469e09db
CHOITAEHYUNG <cth400@naver.com>
2021-01-17 20:53:38 +0100

    Initial Commit

---
+++ State-Binding/State_BindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  State_BindingApp.swift
+//  State-Binding
+//
+//  Created by TAEHYUNG CHOI on 17/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct State_BindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
