e012fb52896d06f1d5b7c34edd0a0f88256de48f
CHOITAEHYUNG <cth400@naver.com>
2021-01-17 21:32:55 +0100

    choi

--- State-Binding/ContentView.swift
+++ State-Binding/ContentView.swift
@@ -8,11 +8,40 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var text = "test"
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            
+            Text(self.text)
+                .padding()
+            Button(action: {
+                self.text = "change text"
+            }, label: {
+                Text("Button")
+            })
+            TestButtonView(text: $text)
+        }
     }
 }
+
+
+
+struct TestButtonView : View {
+    
+    @Binding var text : String
+    
+    var body: some View {
+        Button(action: {
+            text = "change Text"
+        }, label: {
+            Text("TestButton")
+        })
+    }
+}
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
